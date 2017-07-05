93693f8270b24d3ec47ea47dfa11c5c736449b6b
bozsob <bozso.beatrix@gmail.com>
2017-07-05 11:48:16 +0200

    added: HasReadonlyAttribute()

---
+++ noname-SanityArchiver/FileAttributeChange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace noname_SanityArchiver
+{
+    class FileAttributeChange
+    {
+
+        public FileAttributeChange() { }
+
+        // get file attributes
+        public FileAttributes GetFileAttributes(string filePath)
+        {
+            FileAttributes attributes = File.GetAttributes(filePath);
+            return attributes;
+        }
+
+        // check whether a file has readonly attribute
+        public bool HasReadonlyAttribute(string filePath)
+        {
+            bool isReadOnly = ((File.GetAttributes(filePath) & FileAttributes.ReadOnly)
+            == FileAttributes.ReadOnly);
+
+            if (isReadOnly == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }   
+}
