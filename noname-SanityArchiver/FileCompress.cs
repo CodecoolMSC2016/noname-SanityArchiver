190f2921e58f688aed7c5719c9b2c02628c277c5
bozsob <bozso.beatrix@gmail.com>
2017-07-06 10:57:29 +0200

    FileCompress class modified: new methods added previous methods modified: names changed, return value changed

--- noname-SanityArchiver/FileCompress.cs
+++ noname-SanityArchiver/FileCompress.cs
@@ -11,62 +11,121 @@ namespace noname_SanityArchiver
 {
     class FileCompress
     {
-        private List<string> filesToCompress;
-
-        public FileCompress() { }
-
-        public bool CheckSelectedItemIsFolder(List<string> filesToCompress)
+        public string CreateZipFromOneSelectedItem(string item, string zipFileName)
         {
-            if (filesToCompress.Count == 1)
+            string itemPath = Path.GetFullPath(item);
+            string zipPath;
+
+            if (File.Exists(itemPath))
             {
-                string path = filesToCompress[0];
-                if (File.Exists(path))
+                // compress the file
+                zipPath = Path.GetDirectoryName(itemPath) + "\\" + zipFileName + ".zip";
+                ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Create);
+                zip.CreateEntryFromFile(item, Path.GetFileName(item), CompressionLevel.Optimal);
+
+                zip.Dispose();
+
+                return zipPath;
+            }
+            else if (Directory.Exists(itemPath))
+            {
+                string folderPath = Path.GetFullPath(item);
+
+                zipPath = Path.GetDirectoryName(folderPath) + "\\" + zipFileName + ".zip";
+
+                // compress the folder
+                ZipFile.CreateFromDirectory(folderPath, zipPath);
+                return zipPath;
+            }
+            return null;
+        }
+
+        public string CreateZipFromMoreSelectedItems(string[] items, string zipFileName)
+        {
+            if (this.CheckIfSelectedItemsContainsFolder(items))
+            {
+                // create temporary folder
+                string folderName = items[0];
+                string folderPath = Path.GetFullPath(folderName);
+
+                string tempFolderPath = Path.Combine(Path.GetDirectoryName(folderName), "tempFolder");
+
+                Directory.CreateDirectory(tempFolderPath);
+
+                // put file(s) and folder(s) to the temporary folder
+                foreach (string i in items)
                 {
-                    return false;
+                    if (File.Exists(Path.GetFullPath(i)))
+                    {
+                        string fileName = System.IO.Path.GetFileName(i);
+                        string destFile = System.IO.Path.Combine(tempFolderPath, fileName);
+                        File.Copy(i, destFile, true);
+                    }
+                    if (Directory.Exists(Path.GetFullPath(i)))
+                    {
+                        CopyDirectory(Path.GetFullPath(i), tempFolderPath);
+                    }
+
                 }
-                else if (Directory.Exists(path))
+
+                string zipPath = Path.GetDirectoryName(folderPath) + "\\" + zipFileName + ".zip";
+
+                // compress the temporary folder as a single folder
+                ZipFile.CreateFromDirectory(tempFolderPath, zipPath);
+
+                return zipPath;
+            }
+            else
+            {
+                string fileName = items[0];
+                string startPath = Path.GetDirectoryName(fileName);
+                string zipPath = startPath + "\\" + zipFileName + ".zip";
+
+                // compress file(s)
+                ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Create);
+
+                foreach (string i in items)
+                {
+                    zip.CreateEntryFromFile(i, Path.GetFileName(i), CompressionLevel.Optimal);
+                }
+                zip.Dispose();
+
+                return zipPath;
+            }
+
+        }
+
+        public bool CheckIfSelectedItemsContainsFolder(string[] items)
+        {
+            foreach (string i in items)
+            {
+                if (Directory.Exists(Path.GetDirectoryName(i)))
                 {
                     return true;
-                }
-                else
-                {
-                    // path doesn't exist
-                    Debug.WriteLine("Path doesn't exist.");
                 }
             }
             return false;
         }
 
-        public void Compress(List<string> filesToCompress, string zipFileName)
+
+        private static void CopyDirectory(string root, string dest)
         {
-            
-            if (this.CheckSelectedItemIsFolder(filesToCompress))
+            foreach (var directory in Directory.GetDirectories(root))
             {
-                string folderName = filesToCompress[0];
-                string startPath = Path.GetFullPath(folderName);
-                string zipPath = Path.GetDirectoryName(startPath) + "\\" + zipFileName + ".zip";
-
-                // compress the folder
-                ZipFile.CreateFromDirectory(startPath, zipPath);
-            }
-            else
-            {
-                string fileName = filesToCompress[0];
-                string startPath = Path.GetDirectoryName(fileName);
-                string zipPath = startPath + "\\" + zipFileName + ".zip";
-
-                // compress file(s)
-                string[] files = filesToCompress.ToArray();
-
-                ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Create);
-                foreach (string file in files)
+                string dirName = Path.GetFileName(directory);
+                if (!Directory.Exists(Path.Combine(dest, dirName)))
                 {
-                    zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
-
+                    Directory.CreateDirectory(Path.Combine(dest, dirName));
                 }
-                zip.Dispose();
-            } 
+                CopyDirectory(directory, Path.Combine(dest, dirName));
+            }
+
+            foreach (var file in Directory.GetFiles(root))
+            {
+                File.Copy(file, Path.Combine(dest, Path.GetFileName(file)));
+            }
         }
+
 
         public void DeCompress(string fileToDecompress, string folderToExtract)
         {
