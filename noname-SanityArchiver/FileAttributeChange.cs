b74c47d639fcb64a259f0e9ee5b2cd15ba240b49
Imre Meszesan <imre.meszesan@gmail.com>
2017-07-06 12:42:05 +0200

    simplified FileAttributeChange class

--- noname-SanityArchiver/FileAttributeChange.cs
+++ noname-SanityArchiver/FileAttributeChange.cs
@@ -12,42 +12,18 @@ namespace noname_SanityArchiver
 
         public FileAttributeChange() { }
 
-        // get file attributes
-        public void GetFileAttributes(string filePath)
-        {
-            FileAttributes attributes = File.GetAttributes(filePath);
-        }
-
         // check whether a file has readonly attribute
         public bool HasReadonlyAttribute(string filePath)
         {
-            bool isReadOnly = ((File.GetAttributes(filePath) & FileAttributes.ReadOnly)
+            return ((File.GetAttributes(filePath) & FileAttributes.ReadOnly)
             == FileAttributes.ReadOnly);
-
-            if (isReadOnly == true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-
         }
+
         // check whether a file has hidden attribute
         public bool HasHiddenAttribute(string filePath)
         {
-            bool isHidden = ((File.GetAttributes(filePath) & FileAttributes.Hidden)
+            return ((File.GetAttributes(filePath) & FileAttributes.Hidden)
                 == FileAttributes.Hidden);
-
-            if (isHidden == true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
 
         // set readonly attribute (no other attribute will set)
